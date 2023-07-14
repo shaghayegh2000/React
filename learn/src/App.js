cf86c201020d63a8787cf3d71bfd165bf818ffd5
shaghaygh <shqayqm145@gmail.com>
2023-07-14 22:59:18 +0330

    14/7/2023 , 22:50

--- learn/src/App.js
+++ learn/src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import Counter from './component/Counter';
 // import { Component } from 'react';
+import { useState } from 'react';
 
-const App = () => {
-  return (
-    <>
-      <div className='App'>
-        <h2>My counter</h2>
-        <Counter/>
-      </div>
-    </>
-  );
-}
+// const App = () => {
+//   return (
+//     <>
+//       <div className='App'>
+//         <h2>My counter</h2>
+//         <Counter count={5}/>
+//         <Counter>
+//           this is a counter!
+//         </Counter>
+//       </div>
+//     </>
+//   );
+// }
 
 // class component
 // class App extends Component {
@@ -26,6 +30,61 @@ const App = () => {
 //   );
 //   }
 // }
+
+
+// learn state in class and function
+// class App extends Component {
+
+  // state = {
+  //   count: 0,
+  //   name: "Shaghayegh",
+  //   Lname: "Mohamadi"
+  // }
+
+//   render(){
+//     const{count , name , Lname} = this.state;
+//     return(
+//       <div className='App'>
+//         <header>
+//           <h2>My Counter</h2>
+//         </header>
+//         <p>{count}</p>
+//         <p>{name} {Lname}</p>
+//       </div>
+//     )
+//   }
+// }
+
+const App = () => { 
+  // statefuland stateles 
+  const[count , setCount] = useState(0);
+  const[name , setName] = useState("Shaghayegh");
+
+  const changeCount = () => {
+    setCount(5);
+  }
+  const increaseCount = () => {
+    setCount(count+1)
+  }
+  const decreaseCount = () => {
+    setCount(count-1)
+  }
+  const resetCount = () => {
+    setCount(0)
+  }
+
+  return (
+    <div className='App'>
+      <header>
+        <h2>My Counter</h2>
+      </header>
+      <Counter/>
+      <p>{count}</p>
+      <p>{name}</p>
+      <button className='btn' onClick={changeCount}>change count</button>
+    </div>
+  )
+}
 
 
 export default App;
--- learn/src/component/Counter.jsx
+++ learn/src/component/Counter.jsx
@@ -1,22 +1,43 @@
-// import { Component } from "react";
+import { Component } from "react";
 
-const Counter = () => {
-    return(
-        <>
-            <p>counter : {Math.floor(Math.random()*1000)}</p>
-        </>
-    )
-}
+
+// const Counter = (props) => {
+//     return(
+//         <>
+//             <p>counter : {Math.floor(Math.random()*1000)}</p>
+//             <p>{props.count || 20} : is a prop!</p>
+//             <p>{props.children} and {props.count} : is a prop!</p>
+//         </>
+//     )
+// }
+// Counter.defaultProps = {count: 25}
+
 
 // class component
 // class Counter extends Component {
+//     static defaultProps = {
+//         count: 100
+//     }
 //     render() {
+//         const {count} = this.props;
 //         return (
 //             <>
 //                 <p>counter : {Math.floor(Math.random()*1000)}</p>
+//                 <p>counter : {count}</p>
+//                 <h3>{count} is a prop!</h3>
 //             </>
 //         )
 //     }
 // }
+
+class Counter extends Component {
+    render() {
+        return (
+            <div>
+                <p>Counter</p>
+            </div>
+        )
+    }
+}
 
 export default Counter;
