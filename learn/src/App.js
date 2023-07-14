29dc3636f205f04ff935665da2e401d29b92e7a8
shaghaygh <shqayqm145@gmail.com>
2023-07-14 20:28:13 +0330

    14/7/2023 , 20:28

---
+++ learn/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Counter from './component/Counter';
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <h2>My counter</h2>
+        <Counter/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ learn/src/component/Counter.jsx
@@ -0,0 +1,9 @@
+const Counter = () => {
+    return(
+        <>
+            <p>counter : {Math.floor(Math.random()*1000)}</p>
+        </>
+    )
+}
+
+export default Counter;
