3dde6b1a0aa5ab0ea1e4a32c051e061e69569242
shaghaygh <shqayqm145@gmail.com>
2023-07-31 12:22:14 +0330

    31/7/2023 , 12:04

--- contact-manager-app/src/App.js
+++ contact-manager-app/src/App.js
@@ -2,14 +2,25 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Contacts from './components/contact/Contacts';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import AddContact from '../src/components/AddContact';
+import ViewContact from '../src/components/ViewContact.jsx';
+import EditContact from '../src/components/EditContact'
 
 const App = () => {
-  const [loading , setLoading] = useState(false);
-  const [getContacts , setContacts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [getContacts, setContacts] = useState([]);
   return (
     <>
-      <Navbar/>
-      <Contacts contacts={getContacts} loading={loading}/>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Navigate to="/contacts" />}>ّ</Route>
+        <Route path='/contacts' element={<Contacts contacts={getContacts} loading={loading}/>}></Route>
+        <Route path='/contacts/add' element={<AddContact />}></Route>
+        <Route path='/contacts/:contactId' element={<ViewContact />}></Route>
+        <Route path='/contacts/edit/:contactId' element={<EditContact />}></Route>
+      </Routes>
+      {/* <Contacts contacts={getContacts} loading={loading}/> */}
     </>
   )
 }
---
+++ react-lifecycle/src/App.js
@@ -0,0 +1,95 @@
+import './App.css';
+import Counter from './component/Counter';
+// import { Component } from 'react';
+import { useState } from 'react';
+
+// const App = () => {
+//   return (
+//     <>
+//       <div className='App'>
+//         <h2>My counter</h2>
+//         <Counter count={5}/>
+//         <Counter>
+//           this is a counter!
+//         </Counter>
+//       </div>
+//     </>
+//   );
+// }
+
+// class component
+// class App extends Component {
+//   render() {
+//       return (
+//     <>
+//       <div className='App'>
+//         <h2>My counter</h2>
+//         <Counter/>
+//       </div>
+//     </>
+//   );
+//   }
+// }
+
+
+// learn state in class and function
+// class App extends Component {
+
+  // state = {
+  //   count: 0,
+  //   name: "Shaghayegh",
+  //   Lname: "Mohamadi"
+  // }
+
+//   render(){
+//     const{count , name , Lname} = this.state;
+//     return(
+//       <div className='App'>
+//         <header>
+//           <h2>My Counter</h2>
+//         </header>
+//         <p>{count}</p>
+//         <p>{name} {Lname}</p>
+//       </div>
+//     )
+//   }
+// }
+
+const App = () => { 
+  // statefuland stateles 
+  const[count , setCount] = useState(0);
+  const[name , setName] = useState("Shaghayegh");
+
+  const changeCount = () => {
+    setCount(5);
+  }
+  const increaseCount = () => {
+    setCount(count+1)
+  }
+  const decreaseCount = () => {
+    if (count>0) {
+      setCount(count-1)
+    }
+  }
+  const resetCount = () => {
+    setCount(0)
+  }
+  
+  //inline styling
+  // const myStyle = {border: '1px solid black'}
+
+  return (
+    <div className='App'>
+      <header>
+        <h2 style={{color:'blue'}}>My Counter</h2>
+      </header>
+      <Counter inc={increaseCount} dec={decreaseCount} res={resetCount} count={count}/>
+      <p>{count}</p>
+      {/* <p>{name}</p> */}
+      <button className='btn' onClick={changeCount}>change count</button>
+    </div>
+  )
+}
+
+
+export default App;
