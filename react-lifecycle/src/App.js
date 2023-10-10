f3cfc342ebaf63671686733c31feef6f65a47e71
shaghaygh <shqayqm145@gmail.com>
2023-10-10 14:26:11 +0330

    10/10/2023 , 14:03

--- react-lifecycle/src/App.js
+++ react-lifecycle/src/App.js
@@ -1,11 +1,14 @@
 import { Component } from "react";
 import "./App.css";
+import Clock from "./component/Clock";
 
 class App extends Component {
     constructor() {
         super();
         console.log("App.js - constructor()");
         this.state = { date: new Date(), showClock: true, color: false };
+        this.colorChange = this.colorChange.bind(this);
+        this.clockChange = this.clockChange.bind(this);
     }
 
     static getDerivedStateFromProps(props, state) {
@@ -13,16 +16,46 @@ class App extends Component {
         return state;
     }
 
-    componentDidMount(){
+    componentDidMount() {
         console.log("App.js - componentDidMount()");
+        this.timer = setInterval(() => {
+            this.tick();
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        console.log("App.js - componentWillUnmount()");
+        clearInterval(this.timer);
+    }
+
+    tick() {
+        this.setState({ date: new Date() });
+    }
+
+    clockChange() {
+        this.setState({ showClock: !this.state.showClock });
+    }
+
+    colorChange() {
+        this.setState({ color: !this.state.color });
     }
 
     render() {
         console.log("App.js - render()");
 
+        const { date, showClock, color } = this.state;
+
         return (
             <div className="App mt-2">
                 <p>Hello World!</p>
+
+                {
+                    showClock ? (
+                        <Clock date={date} />
+                    ) : null
+                }
+                <button type="" onClick={this.clockChange}>Show Clock</button>
+                <button type="" onClick={this.colorChange}>change color</button>
             </div>
         )
     }
--- react-lifecycle/src/component/Clock.jsx
+++ react-lifecycle/src/component/Clock.jsx
@@ -1,5 +1,43 @@
 import { Component } from "react";
 class Clock extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {};
+        console.log("Clock.jsx - constructor()");
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log("Clock.jsx - getDerivedStateFromProps()");
+        return state;
+    }
+
+    shouldComponentUpdate(nextProp , nextState) {
+        return true;
+    }
+
+    componentDidMount() {
+        console.log("Clock.jsx - componentDidMount()");
+    }
+
+    componentWillUnmount() {
+        console.log("Clock.jsx - componentWillUnmount()");
+    }
+
+    getSnapshotBeforeUpdate(preProp , prevState){
+        console.log("Clock.jsx - getSnapshotBeforeUpdate()");
+    }
+
+    render() {
+        console.log("Clock.jsx - render()");
+
+        const { date } = this.props;
+
+        return (
+            <>
+                <p>Time : {date.toLocaleTimeString()}</p>
+            </>
+        )
+    }
 }
 export default Clock;
